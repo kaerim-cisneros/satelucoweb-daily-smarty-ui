cc71fc9f27ba1613f769f41e6799dc27e828c59c
kaerim-cisneros <kaerim.cisneros@gmail.com>
2019-07-15 23:18:08 -0600

    modified fetchpostswithquery actionCreator to take in an optional callback

--- src/actions/index.js
+++ src/actions/index.js
@@ -26,7 +26,7 @@ export function fetchPostsWithQuery(query, callback) {
                     type: SET_RESULTS_POSTS,
                     payload: response.data.posts
                 })
-                //if(callback) { callback() }
+                if(callback) { callback() }
             })
     }
 }
